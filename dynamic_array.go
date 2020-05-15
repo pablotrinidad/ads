ed39ded7a33e8c8cbb9b3dc42b06a7ab49e548eb
Pablo Trinidad <p@pablotrinidad.me>
2020-05-15 16:33:40 -0500

    Renamed Dynamic Array DS from List to Array

--- dynamic_array.go
+++ dynamic_array.go
@@ -7,62 +7,62 @@ import (
 	"strings"
 )
 
-const listDefaultCapacity int = 4
+const arrayDefaultCapacity int = 4
 
-// List implements a dynamic array data structure, just like built-in slices.
-type List struct {
+// Array implements a dynamic array data structure, just like built-in slices.
+type Array struct {
 	length   int
 	capacity int
 	data     []interface{}
 }
 
-// NewList returns a newly created list of length 0.
-func NewList() List {
-	return List{}
+// NewArray returns a newly created array of length 0.
+func NewArray() Array {
+	return Array{}
 }
 
-// Add appends a new element to the list.
-func (l *List) Add(v interface{}) {
-	if l.length == l.capacity {
-		l.resize()
+// Add appends a new element to the init.
+func (a *Array) Add(v interface{}) {
+	if a.length == a.capacity {
+		a.resize()
 	}
-	l.data[l.length] = v
-	l.length++
+	a.data[a.length] = v
+	a.length++
 }
 
-// Remove an element of the container (if exists).
-func (l *List) Remove(v interface{}) {
-	for i := l.length - 1; i >= 0; i-- {
-		if l.data[i] == v {
-			l.RemoveIth(i)
+// Remove an element of the array (if exists).
+func (a *Array) Remove(v interface{}) {
+	for i := a.length - 1; i >= 0; i-- {
+		if a.data[i] == v {
+			a.RemoveIth(i)
 		}
 	}
 }
 
 // RemoveIth removes the ith element of the array.
-func (l *List) RemoveIth(i int) error {
-	if !l.validIndex(i) {
+func (a *Array) RemoveIth(i int) error {
+	if !a.validIndex(i) {
 		return fmt.Errorf("index %d out of range", i)
 	}
-	copy(l.data[i:], l.data[i+1:l.length])
-	l.data[l.length-1] = nil
-	l.length--
+	copy(a.data[i:], a.data[i+1:a.length])
+	a.data[a.length-1] = nil
+	a.length--
 	return nil
 }
 
 // Get returns the ith-element of the array.
-func (l *List) Get(i int) (interface{}, error) {
-	if !l.validIndex(i) {
+func (a *Array) Get(i int) (interface{}, error) {
+	if !a.validIndex(i) {
 		return nil, fmt.Errorf("index %d out of range", i)
 	}
-	return l.data[i], nil
+	return a.data[i], nil
 }
 
-// Contains returns whether an element is in the container or not. Implementation is linear search
+// Contains returns whether an element is in the array or not. Implementation is linear search
 // since no assumptions can be made about the order of the data.
-func (l *List) Contains(v interface{}) bool {
-	for i := 0; i < l.length; i++ {
-		if l.data[i] == v {
+func (a *Array) Contains(v interface{}) bool {
+	for i := 0; i < a.length; i++ {
+		if a.data[i] == v {
 			return true
 		}
 	}
@@ -70,29 +70,29 @@ func (l *List) Contains(v interface{}) bool {
 }
 
 // Iterator returns an array iterator
-func (l *List) Iterator() Iterable {
-	return ListIterable{i: 0, l: l}
+func (a *Array) Iterator() Iterable {
+	return ArrayIterable{i: 0, a: a}
 }
 
 // Size returns the length of the array.
-func (l *List) Size() int {
-	return int(l.length)
+func (a *Array) Size() int {
+	return int(a.length)
 }
 
 // Empty removes all the elements in the array.
-func (l *List) Empty() {
-	l.length = 0
-	l.capacity = 0
-	l.data = nil
+func (a *Array) Empty() {
+	a.length = 0
+	a.capacity = 0
+	a.data = nil
 }
 
 // Stringer returns a string representation of the array content.
-func (l List) String() string {
+func (a Array) String() string {
 	var b strings.Builder
 	b.WriteString("[")
-	for i := 0; i < l.length; i++ {
-		b.WriteString(fmt.Sprintf("%v", l.data[i]))
-		if i < l.length-1 {
+	for i := 0; i < a.length; i++ {
+		b.WriteString(fmt.Sprintf("%v", a.data[i]))
+		if i < a.length-1 {
 			b.WriteString(", ")
 		}
 	}
@@ -100,36 +100,36 @@ func (l List) String() string {
 	return b.String()
 }
 
-func (l *List) validIndex(i int) bool {
-	return i >= 0 && i < l.length
+func (a *Array) validIndex(i int) bool {
+	return i >= 0 && i < a.length
 }
 
 // resize internal array by a factor of ~1.125. See CPython's list_resize method for reference
 // https://github.com/python/cpython/blob/master/Objects/listobject.c#L36.
-func (l *List) resize() {
-	switch l.capacity {
+func (a *Array) resize() {
+	switch a.capacity {
 	case 0:
-		l.capacity = listDefaultCapacity
+		a.capacity = arrayDefaultCapacity
 	default:
-		l.capacity = (l.capacity + (l.capacity >> 3) + 6) &^ 3
+		a.capacity = (a.capacity + (a.capacity >> 3) + 6) &^ 3
 	}
-	newData := make([]interface{}, l.capacity)
-	copy(newData, l.data)
-	l.data = newData
+	newData := make([]interface{}, a.capacity)
+	copy(newData, a.data)
+	a.data = newData
 }
 
-// ListIterable implements Iterable interface for List.
-type ListIterable struct {
+// ArrayIterable implements Iterable interface for Array.
+type ArrayIterable struct {
 	i int
-	l *List
+	a *Array
 }
 
 // Scan returns a boolean indicating if there's a next element or not.
-func (i ListIterable) Scan() bool {
-	return i.i < i.l.length
+func (i ArrayIterable) Scan() bool {
+	return i.i < i.a.length
 }
 
 // Next returns the next element in the iterable.
-func (i ListIterable) Next() (interface{}, error) {
-	return i.l.Get(i.i)
+func (i ArrayIterable) Next() (interface{}, error) {
+	return i.a.Get(i.i)
 }
